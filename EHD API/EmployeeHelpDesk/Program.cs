72d7b5da7474a010e2c4a98791226efad107980f
pradeepnagasamudra <131743480+pradeepnagasamudra@users.noreply.github.com>
2024-02-02 09:48:37 +0530

    azure setup

--- EHD API/EmployeeHelpDesk/Program.cs
+++ EHD API/EmployeeHelpDesk/Program.cs
@@ -1,13 +1,30 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using EHD.BAL.Implementations;
 using EHD.BAL.Interface;
 using EHD.BAL.Templates;
+using EHD.DAL.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var keyVaultConfig = builder.Configuration.GetSection("azure");
+var keyVaultUrl = keyVaultConfig["keyvaultUrl"];
+var clientId = keyVaultConfig["clientId"];
+var clientSecret = keyVaultConfig["clientSecret"];
+var tenantId = keyVaultConfig["tenantId"];
 
+ClientSecretCredential clientdetails = new ClientSecretCredential(tenantId, clientId, clientSecret);
+SecretClient secretClient = new SecretClient(new Uri(keyVaultUrl), clientdetails);
+
+var connectionStringSecretName = keyVaultConfig["secretName"];
+var connectionStringSecret = secretClient.GetSecret(connectionStringSecretName);
+var connectionString = connectionStringSecret.Value.Value;
+
+builder.Services.AddDbContext<EHDContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddTransient<IAccount, AccountRepo>();
 builder.Services.AddTransient<IMaster, MasterRepo>();
