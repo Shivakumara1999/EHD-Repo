45d3fbbab5b93e2c4b2ca2c4a4619b553a497e5c
Supritha S <suprithasuresh2@gmail.com>
2024-02-02 16:52:17 +0530

    layout and changepassword

---
+++ EHD Client/ehd_ui/src/layout/layoutSidebar.tsx
@@ -0,0 +1,219 @@
+import React, { useState, useEffect } from "react";
+import { Layout, Menu } from "antd";
+
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import "../global.css";
+import Sider from "antd/es/layout/Sider";
+import { Content } from "antd/es/layout/layout";
+import UserAndChangePassword from "../pages/accounts/changePassword";
+import { MdOutlineGridView } from "react-icons/md";
+import { FaFileAlt } from "react-icons/fa";
+import { HiOutlineViewList, HiUsers } from "react-icons/hi";
+import { FaRegFileAlt } from "react-icons/fa";
+import { FcDataConfiguration } from "react-icons/fc";
+import { BsTicketDetailed } from "react-icons/bs";
+interface MySidebarProps {
+  role: string;
+}
+
+const Sidebar: React.FC<MySidebarProps> = ({ role }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [selectedMenuItem, setSelectedMenuItem] = useState<string | null>(
+    localStorage.getItem("selectedMenuItem") || "1"
+  );
+  const navigate = useNavigate();
+
+  const handleMenuClick = (key: string) => {
+    setSelectedMenuItem(key);
+    localStorage.setItem("selectedMenuItem", key);
+  };
+  useEffect(() => {
+    
+    setSelectedMenuItem(localStorage.getItem("selectedMenuItem") || "1");
+  }, []);
+
+
+  const getMenuItems = () => {
+    switch (role) {
+      case "user":
+        return [
+          <Menu.Item
+            key="1"
+            icon={<MdOutlineGridView  />}
+            onClick={() => handleMenuClick("1")}
+          >
+            <Link to="/overview">Overview</Link>
+          </Menu.Item>,
+          <Menu.Item
+            key="2"
+            icon={<FaFileAlt  />}
+            onClick={() => handleMenuClick("2")}
+          >
+            <Link to="/createTicket">Create Ticket</Link>
+          </Menu.Item>,
+          <Menu.Item
+            key="3"
+            icon={<HiOutlineViewList  />}
+            onClick={() => handleMenuClick("3")}
+          >
+            View Ticket History
+          </Menu.Item>,
+        ];
+      case "admin":
+        return [
+          <Menu.Item
+            key="1"
+            icon={<MdOutlineGridView />}
+            onClick={() => handleMenuClick("1")}
+          >
+            <Link to="/overview">Dashboard</Link>
+          </Menu.Item>,
+          <Menu.Item
+            key="2"
+            icon={<FaRegFileAlt  />}
+            onClick={() => handleMenuClick("2")}
+          >
+            <Link to="/createTicket">Escalations</Link>
+          </Menu.Item>,
+          <Menu.Item
+            key="3"
+            icon={<HiUsers  />}
+            onClick={() => handleMenuClick("3")}
+          >
+         Employees
+          </Menu.Item>,
+          <Menu.Item
+          key="4"
+          icon={<FcDataConfiguration  />}
+          onClick={() => handleMenuClick("3")}
+        >
+         Configuartion
+        </Menu.Item>,
+        <Menu.Item
+        key="5"
+        icon={<FaFileAlt />}
+        onClick={() => handleMenuClick("3")}
+      >
+      Create Ticket
+      </Menu.Item>,
+      <Menu.Item
+      key="6"
+      icon={<HiOutlineViewList />}
+      onClick={() => handleMenuClick("3")}
+    >
+     View Ticket History
+    </Menu.Item>,
+        ];
+      case "department":
+        return [
+          <Menu.Item
+            key="1"
+            icon={<MdOutlineGridView />}
+            onClick={() => handleMenuClick("1")}
+          >
+            Overview
+          </Menu.Item>,
+          <Menu.Item
+            key="2"
+            icon={<BsTicketDetailed  />}
+            onClick={() => handleMenuClick("2")}
+          >
+            Ticketing System
+          </Menu.Item>,
+          <Menu.Item
+            key="3"
+            icon={<FaFileAlt />}
+            onClick={() => handleMenuClick("3")}
+          >
+            Create Ticket
+          </Menu.Item>,
+          <Menu.Item
+            key="4"
+            icon={<HiOutlineViewList />}
+            onClick={() => handleMenuClick("4")}
+          >
+            View Ticket History
+          </Menu.Item>,
+        ];
+      default:
+        return [];
+    }
+  };
+
+  const getContentComponent = () => {
+    switch (role) {
+      case "user":
+        return (
+          <Routes>
+            <Route path="/overview" element={<div>Content for Overview</div>} />
+            <Route
+              path="/create-ticket"
+              element={<div>Content for Create Ticket</div>}
+            />
+            <Route
+              path="/ticket-history"
+              element={<div>Content for View Ticket History</div>}
+            />
+          </Routes>
+        );
+      case "department":
+        return (
+          <Routes>
+            <Route path="/overview" element={<div>Content for Overview</div>} />
+            <Route
+              path="/ticket-system"
+              element={<div>Content for Ticketing System</div>}
+            />
+            <Route
+              path="/create-ticket"
+              element={<div>Content for Create Ticket</div>}
+            />
+            <Route
+              path="/ticket-history"
+              element={<div>Content for View Ticket History</div>}
+            />
+          </Routes>
+        );
+      default:
+        return null;
+    }
+  };
+
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        theme="dark"
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+        className="sidemenus"
+      >
+        <div className="container">
+          <img
+            src="./Images/JoyLogo.png"
+            alt="Logo"
+            className={`images ${collapsed ? "collapsed" : ""}`}
+          />
+        </div>
+        <hr className="line" />
+        <Menu
+          mode="vertical"
+          selectedKeys={[selectedMenuItem || "1"]}
+          defaultSelectedKeys={["1"]}
+          className="menus"
+        >
+          {getMenuItems()}
+        </Menu>
+      </Sider>
+      <Layout>
+        <Content>
+        <UserAndChangePassword/>
+          {getContentComponent()}
+          </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Sidebar;
--- EHD Client/ehd_ui/src/pages/accounts/changepassword.tsx
+++ EHD Client/ehd_ui/src/pages/accounts/changepassword.tsx
@@ -1,10 +1,50 @@
-import React, { useState } from "react";
-import { Form, Input, Button, message } from "antd";
+import { useEffect, useState } from "react";
+import {
+  Avatar,
+  Button,
+  Card,
+  Dropdown,
+  Form,
+  Input,
+  Menu,
+  Modal,
+  Tooltip,
+  message,
+} from "antd";
+import axios from "axios";
 
-const ChangePassword = () => {
+import { RiLockPasswordLine, RiLogoutCircleLine } from "react-icons/ri";
+import { IoLogOutOutline } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { LuUser2 } from "react-icons/lu";
+import "../global.css";
+// import { useNavigate } from "react-router-dom";
+interface UserData {
+  user_Name: string;
+}
+
+const UserAndChangePassword = () => {
+  const [userProfile, setUserProfile] = useState<UserData | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [verificationSent, setVerificationSent] = useState(false);
   const [verificationCode, setVerificationCode] = useState("");
   const [changePasswordEnabled, setChangePasswordEnabled] = useState(false);
+
+  const Email = sessionStorage.getItem("email");
+  //   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (Email) {
+      axios
+        .get(`https://localhost:7120/api/Login/UserProfile?mail_id=${Email}`)
+        .then((response: any) => {
+          setUserProfile(response.data);
+        })
+        .catch((error: any) => {
+          console.error(error.message);
+        });
+    }
+  }, [Email]);
 
   const onFinish = (values:any) => {
     if (verificationSent) {
@@ -24,8 +64,65 @@ const ChangePassword = () => {
     }
   };
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Form
+    <div>
+      <div className="content">
+        
+      <Dropdown
+      overlay={
+        <Menu style={{ minWidth: "200px" }}>
+          <Menu.Item className="changePassword"
+            key="change_password"
+            icon={<RiLockPasswordLine />}
+           
+            onClick={showModal}
+          >
+            Change Password
+          </Menu.Item>
+          <Menu.Item className="logout"
+            key="logout"
+            icon={<IoLogOutOutline />}
+            
+          >
+            Logout
+          </Menu.Item>
+        </Menu>
+      }
+      trigger={["click"]}
+    >
+      <Tooltip title="" className="tooltip-content">
+        <div className="tooltip-avatar">
+          <h2>
+            <LuUser2 className="usericon" />
+          </h2>
+          <p>
+            <strong>User</strong>
+            {/* {userProfile && userProfile.user_Name} */}
+          </p>
+          <IoMdArrowDropdown className="arrowdropicon"/>
+        </div>
+      </Tooltip>
+    </Dropdown>
+        
+      </div>
+      <Modal
+        className="custom-modal"
+        title=""
+        open={isModalOpen}
+        footer={[]}
+        onCancel={handleCancel}
+        width={300}
+        style={{ marginTop: "6%" }}
+      >
+         <Form
       name="changePassword"
       initialValues={{ remember: true }}
       onFinish={onFinish}
@@ -102,7 +199,9 @@ const ChangePassword = () => {
         </Form.Item>
       )}
     </Form>
+      </Modal>
+    </div>
   );
 };
 
-export default ChangePassword;
+export default UserAndChangePassword;
