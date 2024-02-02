9cef650766d1c0b8f7f4dd7cd39cccabac12f078
Chandan0037 <108929093+Chandan0037@users.noreply.github.com>
2024-02-02 10:02:23 +0530

    migartion and mail template

---
+++ EHD API/EHD.BAL/Domain Models/MailTemplateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHD.BAL.Domain_Models
+{
+    public class MailTemplateDTO
+    {
+        public string? ToAddress { get; set; }
+        public string? Subject { get; set; }
+        public string? MailHeader { get; set; }
+        public string? MailBody { get; set; }
+        public string? MailFooter { get; set; }
+    }
+}
